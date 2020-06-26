630e10030493651e46ed9e4a9244fba4278b9a61
Charles Jiang <serach24@users.noreply.github.com>
2020-06-26 15:37:53 -0400

    Add chat button on Nav bar

--- src/components/ui/NavBar/index.js
+++ src/components/ui/NavBar/index.js
@@ -3,14 +3,16 @@ import { Link } from "react-router-dom";
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
+import Dialog from "@material-ui/core/Dialog";
 // import Link from '@material-ui/core/Link';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-
+import ChatOutlinedIcon from '@material-ui/icons/ChatOutlined';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
-import Profile from './../../../pages/Profile';
 
+import Profile from './../../../pages/Profile';
+import Chat from "./../../../components/Chat";
 import "./styles.css";
 import { Drawer } from '@material-ui/core';
 
@@ -54,7 +56,8 @@ function DropMenu() {
 class NavBar extends React.Component {
 
     state = {
-      open:false
+      open:false,
+      chatOpen:false
     }
 
     handleClick = () => {
@@ -71,7 +74,18 @@ class NavBar extends React.Component {
         }
     };
 
+    openChat = () => {
+      this.setState({
+        chatOpen: true
+      })
+    }
 
+    closeChat = () => {
+      this.setState({
+        chatOpen: false
+      })
+    }
+    
     render () {
         const { navbar_root } = this.props;
         return (
@@ -103,6 +117,10 @@ class NavBar extends React.Component {
                                 className="TalkButton" >Admin</Button>
                         <Button component={ Link } to={"/feedback"} color="inherit"
                                 className="TalkButton" >FeedBack</Button>
+                        <ChatOutlinedIcon className="chat-button" onClick={this.openChat}/>
+                        <Dialog open={this.state.chatOpen} onClose={this.closeChat}>
+                          <Chat/>
+                        </Dialog>
                         <Button component={ Link } to={"/login"} color="inherit" 
                                 className="loginButton" >Login</Button>
                         <div className="ProfileButton"> Profile </div>
