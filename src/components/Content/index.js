6abad96e5c98ec23b38aec16f7932110c7d72153
csc-bo <colton2585@outlook.com>
2020-06-26 02:39:18 -0400

    content card update

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
-
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 
@@ -15,15 +19,21 @@ class Content extends React.Component {
     render () {
         const { articleheader, articlecontent } = this.props;
         return(
-            <div className= "article">
-                <div className="block">
+            <Card className= "article">
+                {/* <Card Mediacomponent="img"/> */}
+                <CardActionArea>
+                <CardContent>
                 <Typography variant="h5" noWrap>{articleheader}
                 </Typography>
                 <Typography paragraph>{articlecontent} 
                    </Typography>
+                </CardContent>
+                </CardActionArea>
+                <CardActions>
                 <Button size="small" variant="contained" className="articleButton" > Like </Button>
-                </div>
-            </div>
+                </CardActions>
+                
+            </Card>
         );
     }
 }
