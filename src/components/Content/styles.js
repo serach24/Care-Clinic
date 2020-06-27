e907d979c838d1377d86eadd0546b8c287896cae
csc-bo <colton2585@outlook.com>
2020-06-27 15:22:41 -0400

    update content style

---
+++ src/components/Content/styles.js
@@ -0,0 +1,24 @@
+export const styles = theme => ({
+    root: {
+        border: '1px solid #f5f5f5'
+    },
+    actionarea: {
+        height: 150,
+        display: 'flex',
+    },
+    cardmedia: {
+        width: 120,
+        height:120,
+        border: '1px solid darkgray',
+    },
+    contentarea: {
+        width: 590,
+        height: 120,
+    },
+    content: {
+        overflow: 'hidden',
+        textOverflow: 'ellipsis',
+        whiteSpace: 'nowrap'
+    }
+
+  });
