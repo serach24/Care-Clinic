4dcaaf2dc9b271c84275aba721e8710278882c48
henduMain <ken.cui@mail.utoronto.ca>
2020-06-27 18:16:27 -0400

    Fix some bugs

--- src/pages/AdminPage/index.js
+++ src/pages/AdminPage/index.js
@@ -37,7 +37,7 @@ class AdminPage extends React.Component {
       render() {
         return (
         <div className="WholeAdmin">
-          <NavBar which={this.whichLogInStateAmI()} change={this.changeLogINState}/>
+          <NavBar which={this.props.which} change={this.props.change}/>
               <div className ="FixedNav">
               <button className="NavButton" id = {0} onClick={this.GotoElement.bind(this)}> To Messages </button>
               <button className="NavButton" id = {1} onClick={this.GotoElement.bind(this)}> To Pending Doctors </button>
