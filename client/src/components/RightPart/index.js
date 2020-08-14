066ac9943718c913955eb5acf5a81d87e0da6a95
csc-bo <59941818+csc-bo@users.noreply.github.com>
2020-08-13 23:42:07 -0400

    update img cover

--- client/src/components/RightPart/index.js
+++ client/src/components/RightPart/index.js
@@ -9,8 +9,6 @@ import ListItemText from '@material-ui/core/ListItemText';
 import Collapse from '@material-ui/core/Collapse';
 import ExpandLess from '@material-ui/icons/ExpandLess';
 import ExpandMore from '@material-ui/icons/ExpandMore';
-
-
 import imgforbody from './img/Systems-for-health-topic.jpg';
 
 
